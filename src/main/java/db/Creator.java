91b371b63160cb9842798b6a073a33e00b78689b
gilsuk <devgilsuk@gmail.com>
2020-03-31 16:50:30 +0900

    add class Creator to create DB Scheme

---
+++ src/main/java/db/Creator.java
@@ -0,0 +1,43 @@
+package db;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class Creator {
+	
+	private static final String createKeywordTableSQL = new StringBuilder()
+			.append("CREATE TABLE keyword (")
+			.append("  id INTEGER PRIMARY KEY,")
+			.append("  word TEXT NOT NULL UNIQUE,")
+			.append("  count INTEGER NOT NULL")
+			.append(")")
+			.toString();
+
+	private static final String createCooccurenceTableSQL = new StringBuilder()
+			.append("CREATE TABLE cooccurence (")
+			.append("  key_id INTEGER,")
+			.append("  rel_id INTEGER,")
+			.append("  count INTEGER NOT NULL,")
+			.append("    PRIMARY KEY (key_id, rel_id),")
+			.append("    FOREIGN KEY (key_id)")
+			.append("      REFERENCES keyword (id)")
+			.append("        ON DELETE CASCADE")
+			.append("        ON UPDATE NO ACTION,")
+			.append("    FOREIGN KEY (rel_id)")
+			.append("      REFERENCES keyword (id)")
+			.append("        ON DELETE CASCADE")
+			.append("        ON UPDATE NO ACTION")
+			.append(")")
+			.toString();
+	
+	public boolean createSchemes(Connection connection) {
+		try {
+			connection.prepareStatement(createKeywordTableSQL).execute();
+			connection.prepareStatement(createCooccurenceTableSQL).execute();
+			return true;
+		} catch (SQLException e) {
+			return false;
+		}
+	}
+
+}
