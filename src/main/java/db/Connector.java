e3cf3196d813dac962e783d181a7ad9814da00f0
gilsuk <devgilsuk@gmail.com>
2020-03-31 15:51:56 +0900

    add Class Connector to Singleton DB connection

---
+++ src/main/java/db/Connector.java
@@ -0,0 +1,44 @@
+package db;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import org.sqlite.SQLiteConfig;
+import org.sqlite.SQLiteConfig.JournalMode;
+import org.sqlite.SQLiteConfig.SynchronousMode;
+
+public class Connector {
+	
+	private Connector() {}
+	
+	private static class Factory {
+		public static final Connection connection = new Connector().connect();
+	}
+	
+	public static Connection getConnection() {
+		return Factory.connection;
+	}
+	
+	private Connection connect() {
+		SQLiteConfig config = getConfig();
+		return connectWithConfig(config);
+	}
+
+	private Connection connectWithConfig(SQLiteConfig config) {
+		try {
+			return config.createConnection("jdbc:sqlite::memory:");
+		} catch (SQLException e) { e.printStackTrace(); }
+		
+		return null;
+	}
+
+	private SQLiteConfig getConfig() {
+		SQLiteConfig config = new SQLiteConfig();
+		config.setBusyTimeout(5000);
+		config.setJournalMode(JournalMode.WAL);
+		config.setSynchronous(SynchronousMode.FULL);
+		config.enforceForeignKeys(true);
+		return config;
+	}
+	
+}
